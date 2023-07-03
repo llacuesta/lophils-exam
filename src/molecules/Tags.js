93187a114a519d0cd2f904819657d41cf44e1def
Lyco Lacuesta <lycosheen27@gmail.com>
2023-07-03 12:42:42 +0800

    feat: add Tags molecule

---
+++ src/molecules/Tags.js
@@ -0,0 +1,14 @@
+import Tag from '../atoms/Tag.js'
+import style from '../styles/molecules.module.css'
+
+function Tags({ tags }) {
+    return (
+        <div className={style.tags}>
+            {
+                tags.map((tag, index) => (<Tag tag={tag} key={index}/>))
+            }
+        </div>
+    );
+}
+
+export default Tags;
